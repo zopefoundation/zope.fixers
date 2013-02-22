d501ea31397a8c12587dcb6bc2b2793af8cc62b5
Stephan Richter <stephan.richter@gmail.com>
2013-02-22 18:36:07 -0500

    Python 3.3 too.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(name='zope.fixers',
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Topic :: Software Development :: Libraries :: Python Modules",
 
         ],
