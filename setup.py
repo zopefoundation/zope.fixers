759bc082b115035fdcec21d421c8fa06dbd4c183
Stephan Richter <stephan.richter@gmail.com>
2013-02-22 18:36:47 -0500

    Back to development: 1.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='zope.fixers',
-      version='1.1.0',
+      version='1.2.0.dev0',
       description="2to3 fixers for Zope",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
